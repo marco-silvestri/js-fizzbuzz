260d6259135002b4187a55bbdf596c1ab8fca4a2
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-01 17:13:15 +0200

    HTML Print, no style

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Print 100 nubmbers. Change multiples of 3 with the word of Fizz, mulitples of 5 with Buzz
 
+// References
+var outputField = document.getElementById('app__numbers');
+var outputFieldAdd;
+
+// Declare array and explicit number
 var numbersCollection = [];
 var currentNumber = 0;
 
@@ -7,16 +12,23 @@ for (var i = 0; i < 100; i++) {
   ++currentNumber;
   if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
     numbersCollection.push('FizzBuzz');
+    outputField.innerHTML += '<li>' + 'FizzBuzz' + '</li>';
   }
   else if (currentNumber % 3 == 0) {
     numbersCollection.push('Fizz');
+    outputField.innerHTML += '<li>' + 'Fizz' + '</li>';
   }
   else if (currentNumber % 5 == 0) {
     numbersCollection.push('Buzz');
+    outputField.innerHTML += '<li>' + 'Buzz '+ '</li>' ;
   }
   else {
     numbersCollection.push(currentNumber);
+    outputField.innerHTML += '<li>' + currentNumber + '</li>' ;
   }
 }
 
-console.log(numbersCollection);
+// Print the output
+
+
+//console.log(numbersCollection);
