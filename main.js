a012b934e7fe206d97e632c38f260c6878f3df66
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-02 10:20:38 +0200

    Code cleanup

--- main.js
+++ main.js
@@ -2,28 +2,27 @@
 
 // References
 var outputField = document.getElementById('app__numbers');
-var outputFieldAdd;
 
 // Declare array and explicit number
-var numbersCollection = [];
+//var numbersCollection = [];
 var currentNumber = 0;
 
 for (var i = 0; i < 100; i++) {
   ++currentNumber;
   if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
-    numbersCollection.push('FizzBuzz');
+    //numbersCollection.push('FizzBuzz');
     outputField.innerHTML += '<li class="fizz-buzz">' + 'FizzBuzz' + '</li>';
   }
   else if (currentNumber % 3 == 0) {
-    numbersCollection.push('Fizz');
+    //numbersCollection.push('Fizz');
     outputField.innerHTML += '<li class="fizz">' + 'Fizz' + '</li>';
   }
   else if (currentNumber % 5 == 0) {
-    numbersCollection.push('Buzz');
+    //numbersCollection.push('Buzz');
     outputField.innerHTML += '<li class="buzz">' + 'Buzz '+ '</li>' ;
   }
   else {
-    numbersCollection.push(currentNumber);
+    //numbersCollection.push(currentNumber);
     outputField.innerHTML += '<li>' + currentNumber + '</li>' ;
   }
 }
